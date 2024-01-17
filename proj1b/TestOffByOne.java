f461bfbf7390a80380fa13e3cbf11a9e670f4f59
f1a3h <ihlwer@outlook.com>
2024-01-17 19:27:17 +0800

    fix: add tests for comparation between upper and lower case

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -14,9 +14,11 @@ public class TestOffByOne {
         assertFalse(offByOne.equalChars('1', '4'));
         assertFalse(offByOne.equalChars('a', 'g'));
         assertFalse(offByOne.equalChars('a', 'A'));
+        assertFalse(offByOne.equalChars('a', 'B'));
 
         assertTrue(offByOne.equalChars('p', 'q'));
         assertTrue(offByOne.equalChars('%', '&'));
         assertTrue(offByOne.equalChars('5', '6'));
+        assertTrue(offByOne.equalChars('A', 'B'));
     }
 }
